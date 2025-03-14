60ec4c9a59980a8b6af1b63eeee08f46e1c22474
Shah Wali <69451223+shahwalidjango@users.noreply.github.com>
2025-03-14 11:37:16 +0500

    Profile

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from . models import Customer, Category, Product, Order
+from . models import Customer, Category, Product, Order, Profile
+from django.contrib.auth.models import User
+
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+
+# profile info and user info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+# unr
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+
+
+
+
+
--- store/models.py
+++ store/models.py
@@ -1,8 +1,32 @@
 import datetime
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    zipcode = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -22,7 +46,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=100)
 
     def __str__(self):
-        return  f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Product(models.Model):
